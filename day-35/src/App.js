50df92a753eefccf52ed0efc485c6dc4b8a1c617
WkhongorzulW <bkhongorzul064@gmail.com>
2023-01-04 11:23:48 +0800

    product list by props

--- day-35/src/App.js
+++ day-35/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+function Profile({img, name, age, email}){
+  return(
+    <div className="profile">
+      <img src={img} alt="profile image"></img>
+      <p>name: {name}</p>
+      <p>age: {age}</p>
+      <div>email: {email}</div>
+    </div>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Profile
+        img="img/download.jpeg"
+        name="Herro Fiennes Tiffin"
+        age={25}
+        email="hero.tiffin@gmail.com"
+      />
+      <Profile
+        img="img/download (1).jpeg"
+        name="Josephine Langford"
+        age={23}
+        email="jose.langford@gmail.com"
+      />
     </div>
   );
 }
