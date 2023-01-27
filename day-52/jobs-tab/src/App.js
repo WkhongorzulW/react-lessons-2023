8b5104f96c6bc3355b8eb5ff06c137d5db6e199b
WkhongorzulW <bkhongorzul064@gmail.com>
2023-01-27 21:08:04 +0800

    tabs

--- day-52/jobs-tab/src/App.js
+++ day-52/jobs-tab/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import { AiFillCaretRight } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
+import CompanyContent from "./components/CompanyContent";
 
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
   const url = "https://course-api.com/react-tabs-project";
 
   useEffect(() => {
@@ -25,46 +25,18 @@ function App() {
       <h1 className="title">Experience</h1>
       <div className="job">
         <div className="company-tab">
-          <Link to={"/john"}>
-            <h4>John</h4>
-          </Link>
-          <Link to={"/smith"}>
-            <h4>Smith</h4>
-          </Link>
-          <Link to={"/tom"}>
-            <h4>Tom</h4>
-          </Link>
+          {data.map((company, index) => {
+            return (
+              <Link to={`${company.id}`} state={company}>
+                <h4 key={index}>{company.company}</h4>
+              </Link>
+            );
+          })}
         </div>
-        <div className="company-content">
-          {}
-          <h2 className="job-title">Full Stack Web Developer</h2>
-          <p className="company-name">John</p>
-          <p className="date">December 2015 - Present</p>
-          <div className="text">
-            <AiFillCaretRight />
-            <p>
-              Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke
-              leggings offal cold-pressed brunch neutra. Hammock photo booth
-              live-edge disrupt.
-            </p>
-          </div>
-          <div className="text">
-            <AiFillCaretRight />
-            <p>
-              Post-ironic selvage chambray sartorial freegan meditation.
-              Chambray chartreuse kombucha meditation, man bun four dollar toast
-              street art cloud bread live-edge heirloom.
-            </p>
-          </div>
-          <div className="text">
-            <AiFillCaretRight />
-            <p>
-              Butcher drinking vinegar franzen authentic messenger bag copper
-              mug food truck taxidermy. Mumblecore lomo echo park readymade
-              iPhone migas single-origin coffee franzen cloud bread tilde vegan
-              flexitarian.
-            </p>
-          </div>
+        <div>
+          <Routes>
+            <Route path={"/:id"} element={<CompanyContent data={data} />} />
+          </Routes>
         </div>
       </div>
     </section>
--- day-52/jobs-tab/src/components/CompanyContent.jsx
+++ day-52/jobs-tab/src/components/CompanyContent.jsx
@@ -1,1 +1,36 @@
-export default function CompanyContent()
+import { AiFillCaretRight } from "react-icons/ai";
+import { useLocation, useParams } from "react-router-dom";
+
+export default function CompanyContent({ data }) {
+  const { id } = useParams();
+  const location = useLocation();
+
+  const company = data.filter((company) => company.id == id);
+  return (
+    <div>
+      {company &&
+        company.map((c, idx) => {
+          return (
+            <div
+              className="company-content"
+              id={c.id}
+              order={c.order}
+              key={idx}
+            >
+              <h2 className="job-title">{c.title}</h2>
+              <p className="company-name">{c.company}</p>
+              <p className="date">{c.dates}</p>
+              {c.duties.map((item, index) => {
+                return (
+                  <div key={index} className="text">
+                    <AiFillCaretRight />
+                    <p>{item}</p>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+    </div>
+  );
+}
