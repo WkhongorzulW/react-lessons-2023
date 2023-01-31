339e354ca7239022b58fa9aab78569c393e02e1f
WkhongorzulW <bkhongorzul064@gmail.com>
2023-01-31 12:42:52 +0800

    sidebar

---
+++ admin-panel-project/fe/src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import SideBar from "../components/SideBar";
+import Main from "../components/Main";
+import Users from "./User";
+
+export default function Home() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Clipped drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <SideBar />
+      <Users />
+    </Box>
+  );
+}
