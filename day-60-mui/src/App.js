e5235edb36e97f11ff1c58de95a839e4c75d71ef
WkhongorzulW <bkhongorzul064@gmail.com>
2023-02-10 11:30:24 +0800

    swiper

--- day-60-mui/src/App.js
+++ day-60-mui/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import GridMUI from "./components/GridMUI";
+import { SwiperUI } from "./components/SwiperUI";
 
 function App() {
   return (
     <div className="App">
       <GridMUI />
+      <SwiperUI />
     </div>
   );
 }
