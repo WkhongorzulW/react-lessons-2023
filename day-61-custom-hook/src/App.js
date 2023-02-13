855ac27cd154d9e692e3c0af512bc3151e666e8f
WkhongorzulW <bkhongorzul064@gmail.com>
2023-02-13 12:43:20 +0800

    theme

---
+++ day-61-custom-hook/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Body from "./components/Body";
+import ChangeTheme from "./components/ChangeTheme";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Day-61 Custom Hook</h1>
+      <ChangeTheme />
+      <Header />
+      <Body />
+      <Footer />
+      {/* <TimerContextProvider>
+        <TimersDashboard />
+      </TimerContextProvider> */}
+    </div>
+  );
+}
