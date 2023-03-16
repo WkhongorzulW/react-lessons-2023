569ec98fac88d9983ab1b95d83c9034b1376f32d
WkhongorzulW <bkhongorzul064@gmail.com>
2023-03-16 10:07:03 +0800

    mongodb crud api

--- day-77-mongoose-setup/backend/routes/api.js
+++ day-77-mongoose-setup/backend/routes/api.js
@@ -8,6 +8,40 @@ api_router.get("/users", async (request, response) => {
   response.json({ data: result });
 });
 
+/*-------------- GET user by id---------------*/
+api_router.get("/user", async (request, response) => {
+  const userId = request.query.id;
+  console.log(userId);
+
+  const user = await User.findOne({ _id: userId });
+
+  response.json({
+    data: user,
+  });
+});
+
+/*-------------- GET user by email---------------*/
+api_router.get("/userByEmail", async (request, response) => {
+  const email = request.query.email;
+  console.log(email);
+
+  const foundUser = await User.find({ email: email }, "_id email name", {
+    sort: { lastLogin: 1 },
+  }).exec();
+
+  response.json({
+    data: foundUser,
+  });
+});
+
+api_router.get("/userGetEmail", async (request, response) => {
+  const userEmail = request.query.email;
+
+  const foundUser = await User.findUserByEmail(userEmail);
+
+  response.json({ data: foundUser });
+});
+
 api_router.post("/user", async (request, response) => {
   const body = request.body;
 
@@ -26,6 +60,15 @@ api_router.delete("/user", async (request, response) => {
   console.log(body);
 
   const result = await User.findOneAndDelete({ _id: body.id });
+
+  response.json({ data: result });
+});
+
+api_router.put("/updateUser", async (request, response) => {
+  const result = await User.updateOne(
+    { email: "khongoroo@gmail.com" },
+    { $set: { lastLogin: Date.now() } }
+  );
 
   response.json({ data: result });
 });
